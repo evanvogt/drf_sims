cce0684edb161ff521baee2f0fd1e3964f4e44bb
Ellie Van Vogt <e.van-vogt@imperial.ac.uk>
2025-03-11 18:09:51 +0000

    finalised binary sim code

---
+++ profiling/profile_5000.sh
@@ -0,0 +1,15 @@
+#PBS -l walltime=7:00:00  
+#PBS -l select=1:ncpus=50:ompthreads=50:mem=100gb
+#PBS -N profile_5000
+
+module purge
+module add tools/prod
+module add R/4.2.1-foss-2022a
+
+eval "$(~/miniforge3/bin/conda shell.bash hook)"
+conda activate drf-env
+
+# Navigate to the script directory
+cd "/rds/general/user/evanvogt/projects/nihr_drf_simulations/live/scripts/profiling"
+
+Rscript profile_5000.R
