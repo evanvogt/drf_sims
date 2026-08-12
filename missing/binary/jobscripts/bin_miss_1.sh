86b355edf35bf7d57cca1ea7ab0df108c57a32b8
Ellie Van Vogt <e.van-vogt@imperial.ac.uk>
2026-08-12 09:19:56 +0100

    sh updates

--- missing/binary/jobscripts/bin_miss_1.sh
+++ missing/binary/jobscripts/bin_miss_1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#PBS -l walltime=02:00:00  
-#PBS -l select=1:ncpus=2:ompthreads=2:mem=10gb
 # the full 9900-row grid from bin_miss_config.R. This used to be 1-1100, the
 # size AFTER bin_miss_analysis.R filtered to the complete_data arm - the filter
 # that renumbered every index (bug D). To run one arm, use
 #   grid_indices(study, method = "complete_data")
+#PBS -l walltime=01:00:00
+#PBS -l select=1:ncpus=1:ompthreads=1:mem=2gb
 #PBS -J 1-9900%190
 #PBS -N bin_miss_1
 #PBS -o logs_1/
@@ -21,4 +21,4 @@ conda activate sim-env
 cd "${PBS_O_WORKDIR}/.."
 
 # Run R script with parameters
-Rscript bin_miss_analysis.R "$PBS_ARRAY_INDEX"
+Rscript bin_miss_analysis.R "$PBS_ARRAY_INDEX" 1 1
